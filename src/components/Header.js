b9621f2bacd2917e16471b1a2b383fddd45a02f9
Deybys Roman <romandeybys@gmail.com>
2021-06-03 13:09:00 -0400

    commit pokeAPI

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="App-topbar">
+            <center>
+                <h1>Pokédex</h1>
+                <h6>Una demostración de React REST API usando la PokéAPI</h6>
+                <a href="https://pokeapi.co/">https://pokeapi.co/</a>
+                <h6>Deybys Román - 2020</h6>
+            </center>
+        </div>
+    )
+};
+
+export default Header
